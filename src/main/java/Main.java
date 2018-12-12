e7417b5e60f9a20f76d2f119c111f987ef690951
Marat_Gabdrakhmanov <gabdra-marat@yandex.ru>
2018-12-12 17:40:03 +0400

    adress

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,11 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
-
+import javax.print.DocFlavor;
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -27,7 +30,7 @@ public class Main {
     public String getToken() {
 
         try {
-            httpGet.setHeader("Cookie","maps_los=0; yandexuid=1086007521541671953; i=jlwVg8fbaUAcmYHi1lQB1x3/BiJmMXkZqdp/609VMZEJMIUUZNBAuzuYk+icdciGgB6/VZIezD2TS87awWbCxJkhXD8=; _ym_uid=1541756039280706898; mda=0; my=YwA=; yandex_gid=44; zm=m-white_bender.webp.css-https%3Awww_zmn5fRGIkJxyNVNvb1YAxfO-1Zw%3Al; fuid01=5c0e5e12415b2171.VjyU2nnhbSoU_QTLClpdaIe-eKX_KOoCViVKWxCTAmO0e153TmIWsmxdc9YF6PlDoeIH4dnP19fivedrkfWVAsaBc8Du6drXABT3POdyTHJP2b9WaWu4eidm0gpSlWI6; yc=1544704662.zen.cach%3A1544449058; _ym_d=1544446000; yabs-frequency=/4/0000000000000000/x5ImS0GpOEfKi704Cs40/; device_id=\"bff6967dcd939252bd9b9c15d28eaf60749bffafa\"; _ym_isad=2; _ym_visorc_1028356=b; yp=1857031953.yrts.1541671953#1857031953.yrtsi.1541671953#1560381034.szm.1:1920x1080:1920x969#1547037458.ygu.1#1545655058.ysl.1#1547124410.csc.1; _ym_wasSynced=%7B%22time%22%3A1544613033884%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D");
+            httpGet.setHeader("Cookie" , "maps_los=0; yandexuid=1086007521541671953; i=jlwVg8fbaUAcmYHi1lQB1x3/BiJmMXkZqdp/609VMZEJMIUUZNBAuzuYk+icdciGgB6/VZIezD2TS87awWbCxJkhXD8=; _ym_uid=1541756039280706898; mda=0; my=YwA=; yandex_gid=44; zm=m-white_bender.webp.css-https%3Awww_zmn5fRGIkJxyNVNvb1YAxfO-1Zw%3Al; fuid01=5c0e5e12415b2171.VjyU2nnhbSoU_QTLClpdaIe-eKX_KOoCViVKWxCTAmO0e153TmIWsmxdc9YF6PlDoeIH4dnP19fivedrkfWVAsaBc8Du6drXABT3POdyTHJP2b9WaWu4eidm0gpSlWI6; yc=1544704662.zen.cach%3A1544449058; _ym_d=1544446000; yabs-frequency=/4/0000000000000000/x5ImS0GpOEfKi704Cs40/; device_id=\"bff6967dcd939252bd9b9c15d28eaf60749bffafa\"; _ym_isad=2; _ym_wasSynced=%7B%22time%22%3A1544613033884%2C%22params%22%3A%7B%22eu%22%3A0%7D%2C%22bkParams%22%3A%7B%7D%7D; yp=1857031953.yrts.1541671953#1857031953.yrtsi.1541671953#1560389008.szm.1:1920x1080:1920x969#1547037458.ygu.1#1545655058.ysl.1#1547124410.csc.1");
             BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             String inputLine = "";
             StringBuffer result = new StringBuffer();
@@ -60,12 +63,24 @@ public class Main {
         }
         return methods;
     }
-    public static void main(String[] args) throws IOException {
+
+    public URL getReqWithParam() throws MalformedURLException, UnsupportedEncodingException {
+        String addressRu = "Ижевск, С.Ковалевской 12";
+        String address = URLEncoder.encode(addressRu, "UTF-8");
+        URL url2 = new URL("https://yandex.ru/maps?text=" + address + "&lang=ru_RU&csrfToken=" + csrfToken);
+        return url2;
+
+    }
+
+
+        public static void main(String[] args) throws IOException {
         Main main = new Main();
         String csrfToken = main.getToken();
         Set<String> getYandexuid = main.getYandexuid();
+        URL getReqWithParam = main.getReqWithParam();
         System.out.println("getYandexuid" +getYandexuid);
         System.out.println(csrfToken);
+        System.out.println(getReqWithParam);
 
 
     }
